452e61fa1878f65651ad0f7bff57f5fe9fffcf74
VikramAdytia <rundarreg@yandex.ru>
2023-02-09 07:40:47 +0300

    final study working class still not beautified

--- src/main/java/FinalClass/Laptop.java
+++ src/main/java/FinalClass/Laptop.java
@@ -1,4 +1,184 @@
 package FinalClass;
 
+import java.util.*;
+
 public class Laptop {
+    private String model;
+    private float diagonal;
+    private String screenResolution;
+    private int hardDiskD;
+    private String operativeMemory;
+    private String operatingSystem;
+    private String color;
+    public Laptop() {
+    }
+    public Laptop(String model, float diagonal, String screenResolution, int hardDisk,
+                  String operativeMemory, String operatingSystem, String color) {
+        this.model = model;
+        this.diagonal = diagonal;
+        this.screenResolution = screenResolution;
+
+        this.hardDiskD = hardDisk;
+        this.operativeMemory = operativeMemory;
+        this.operatingSystem = operatingSystem;
+        this.color = color;
+    }
+    public String getModel() {return model;}
+    public float getDiagonal() {return diagonal;}
+    public String getScreenResolution() {return screenResolution;}
+    public int getHardDiskD() {return hardDiskD;}
+    public String getOperativeMemory() {return operativeMemory;}
+    public String getOperatingSystem() {return operatingSystem;}
+    public String getColor() {return color;}
+
+    public List<Laptop> filter(Set<Laptop> laptop, List<Laptop> laptopList) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Здрасвуйте, Укажите каие параметры для ноут бука вы бы хотели иметь." +
+                "\nУкажите размер жёсткого диска в гб\nВналичии : ");
+
+        Set<Integer> sortedAndDistinctHD = new TreeSet<>();
+        for (Laptop laptopStat : laptopList){
+            sortedAndDistinctHD.add(laptopStat.getHardDiskD());
+        }
+        System.out.println(sortedAndDistinctHD);
+
+        String enterHardDisk = scan.nextLine();
+        int intParseEnterHardDisk = Integer.parseInt(enterHardDisk);
+        System.out.println("Укажите количество оперативной памяти в Гб\nВналичии : ");
+
+        Set<String> sortedAndDistinctRAM = new TreeSet<>();
+        for (Laptop laptopStat : laptopList){
+            sortedAndDistinctRAM.add(laptopStat.getOperativeMemory());
+        }
+        System.out.println(sortedAndDistinctRAM);
+
+        String enterOperativeMemory = scan.nextLine();
+        System.out.println("Укажите операционную систему\nВналичии : ");
+
+        Set<String> sortedAndDistinctSYS = new TreeSet<>();
+        for (Laptop laptopStat : laptopList){
+            sortedAndDistinctSYS.add(laptopStat.getOperatingSystem());
+        }
+        System.out.println(sortedAndDistinctSYS);
+
+        String enterOperatingSystem = scan.nextLine();
+        System.out.println("Укажите цвет ноутбука\nВналичии : ");
+
+        Set<String> sortedAndDistinctColor = new TreeSet<>();
+        for (Laptop laptopStat : laptopList){
+            sortedAndDistinctColor.add(laptopStat.getColor());
+        }
+        System.out.println(sortedAndDistinctColor);
+
+        String enterColor = scan.nextLine();
+
+        List<Laptop> listLaptop = new ArrayList<>();
+        for (Laptop tempLaptop : laptop) {
+            if (intParseEnterHardDisk == tempLaptop.hardDiskD) {
+                if (enterOperativeMemory.equals(tempLaptop.operativeMemory)) {
+                    if (enterOperatingSystem.equals(tempLaptop.operatingSystem)) {
+                        if (enterColor.equals(tempLaptop.color)) {
+                            listLaptop.add(tempLaptop);
+                        }
+                    }
+                }
+            }
+        }
+        return listLaptop;
+    }
+
+    public Set<Laptop> newFilter(Set<Laptop> laptop, List<Laptop> laptopList) {
+        Scanner scan = new Scanner(System.in);
+        Set<Laptop> listLaptop = new HashSet<>(laptop);
+
+        System.out.println("Здрасвуйте, Укажите номер или номера критериев фильрации ноутбуков," +
+                "\n1. Size hard disk\n2. Size HDD \n3. Operating system\n4. Color");
+        String userRequest = scan.nextLine();
+
+        for (int i = 0; i < userRequest.length(); i++) {
+            if(1 == Character.getNumericValue(userRequest.charAt(i))) {
+                System.out.println("Укажите размер жёсткого диска в гб\nВналичии : ");
+
+                // Отдельный метод для создания сета доступных вариантов не делал так как,
+                // всё равно пришлось бы проверять какой критерий и на каждый делать копию с иным .get***
+                // , а переделывать на .get(критерий) уже слишком далеко.
+
+                Set<Integer> sortedAndDistinctHD = new TreeSet<>();
+                for (Laptop laptopStat : laptopList){
+                    sortedAndDistinctHD.add(laptopStat.getHardDiskD());
+                }
+                System.out.println(sortedAndDistinctHD);
+
+
+                String enterHardDisk = scan.nextLine();
+                int intParseEnterHardDisk = Integer.parseInt(enterHardDisk);
+                for (Laptop tempLaptop : laptop) {
+                    if (intParseEnterHardDisk != tempLaptop.hardDiskD){
+                        listLaptop.remove(tempLaptop);
+                    }
+                }
+            }
+
+            if(2 == Character.getNumericValue(userRequest.charAt(i))){
+                System.out.println("Укажите количество оперативной памяти в Гб\nВналичии : ");
+
+                Set<String> sortedAndDistinctRAM = new TreeSet<>();
+                for (Laptop laptopStat : laptopList){
+                    sortedAndDistinctRAM.add(laptopStat.getOperativeMemory());
+                }
+                System.out.println(sortedAndDistinctRAM);
+
+                String enterOperativeMemory = scan.nextLine();
+                for (Laptop tempLaptop : laptop) {
+                    if ((enterOperativeMemory.equals(tempLaptop.operativeMemory)) == false) {
+                        listLaptop.remove(tempLaptop);
+                    }
+                }
+            }
+
+            if(3 == Character.getNumericValue(userRequest.charAt(i))){
+                System.out.println("Укажите операционную систему\nВналичии : ");
+
+                Set<String> sortedAndDistinctSYS = new TreeSet<>();
+                for (Laptop laptopStat : laptopList){
+                    sortedAndDistinctSYS.add(laptopStat.getOperatingSystem());
+                }
+                System.out.println(sortedAndDistinctSYS);
+
+                String enterOperatingSystem = scan.nextLine();
+                for (Laptop tempLaptop : laptop) {
+                    if ((enterOperatingSystem.equals(tempLaptop.operatingSystem) == false)) {
+                        listLaptop.remove(tempLaptop);
+                    }
+                }
+            }
+
+            if(4 == Character.getNumericValue(userRequest.charAt(i))){
+                System.out.println("Укажите цвет ноутбука\nВналичии : black, white");
+
+                Set<String> sortedAndDistinctColor = new TreeSet<>();
+                for (Laptop laptopStat : laptopList){
+                    sortedAndDistinctColor.add(laptopStat.getColor());
+                }
+                System.out.println(sortedAndDistinctColor);
+
+                String enterColor = scan.nextLine();
+                for (Laptop tempLaptop : laptop) {
+                    if ((enterColor.equals(tempLaptop.color)) == false) {
+                        listLaptop.remove(tempLaptop);
+                    }
+                }
+            }
+        }
+        return listLaptop;
+    }
+
+
+    @Override
+    public String toString() {
+        return "\nLaptop  ->  " + model + ",\ndiagonal = " + diagonal + ",\nscreenResolution = "
+                + screenResolution
+                + ",\nhardDisk = " + hardDiskD + ",\noperativeMemory = " + operativeMemory
+                + ",\noperatingSystem = " + operatingSystem + ",\ncolor = " + color + "\n";
+    }
 }
--- src/main/java/FinalClass/Main.java
+++ src/main/java/FinalClass/Main.java
@@ -1,4 +1,67 @@
 package FinalClass;
 
+import java.util.List;
+import java.util.Set;
+
 public class Main {
+    public static void main(String[] args) {
+        Laptop hp_pavilion_g6 = new Laptop("vn_gazebo_d20", 16, "4k",
+                500, "16", "LLIindows", "black");
+        Laptop hp_galaxy_x64 = new Laptop("vn_cosmos_<x>", 14, "FullHD",
+                500, "8", "LLIindows", "white");
+
+        Laptop samsung_a52 = new Laptop("three_stars_52", 18, "FullHD",
+                300, "8", "LLIindows", "black");
+
+        Laptop samsung_m31s = new Laptop("three_stars_31", 16, "4k",
+                1000, "16", "Linux", "black");
+
+        Laptop lenovo_gtx76 = new Laptop("lenin_gtx911", 16, "FullHD",
+                700, "8", "LLIindows", "white");
+
+        Laptop macbook_14pro = new Laptop("dastext_19ora", 17, "4k",
+                1000, "8", "nos", "white");
+
+        Laptop asus_x515 = new Laptop("sus_x515", 17, "FullHD",
+                300, "4", "LLIindows", "black");
+
+
+        Laptop acer_air5 = new Laptop("cera_ar15", 19, "FullHD",
+                600, "8", "LLIindows", "black");
+
+
+        Laptop acer_air6 = new Laptop("cera_ar10" ,18, "4k",
+                500, "16", "LLIindows", "black");
+
+        Laptop acer_a313 = new Laptop("cera_i717", 16, "FullHD",
+                500, "8", "LLIindows", "black");
+
+
+        List<Laptop> laptopList = List.of(hp_galaxy_x64, hp_pavilion_g6, samsung_m31s, samsung_a52, lenovo_gtx76, macbook_14pro, acer_a313, acer_air5, acer_air6, asus_x515);
+
+        //Set <Laptop> laptop = new HashSet<>();
+        Set<Laptop> laptop = Set.of(hp_galaxy_x64, hp_pavilion_g6, samsung_m31s, samsung_a52, lenovo_gtx76, macbook_14pro, acer_a313, acer_air5, acer_air6, asus_x515);
+//        laptop.add(hp_pavilion_g6);
+//        laptop.add(hp_galaxy_x64);
+//        laptop.add(samsung_a52);
+//        laptop.add(samsung_m31s);
+//        laptop.add(lenovo_gtx76);
+//        laptop.add(macbook_14pro);
+//        laptop.add(asus_x515);
+//        laptop.add(acer_air5);
+//        laptop.add(acer_air6);
+//        laptop.add(acer_a313);
+
+
+        Laptop FilterLaptop = new Laptop();
+        // System.out.println("по вашему запросу найдено: " + FilterLaptop.filter(laptop, laptopList));
+
+
+
+        // наверное стоило отобразить уменьшение выбора можно было через выдачу промежуточных результатов фильтрации,
+        // иначе сложно подобрать вариант если больше двух критериев
+        System.out.println(FilterLaptop.newFilter(laptop, laptopList));
+
+    }
+
 }
